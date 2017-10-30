eddd2d5700aa6df2379749606e22f27fb4abcac4
yxn <yanxuning@gmail.com>
2017-10-30 10:30:53 +0800

    测试

--- src/com/fbear/server/StartService.java
+++ src/com/fbear/server/StartService.java
@@ -1,7 +1,11 @@
 package com.fbear.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StartService {
+	private static final Logger logger = LoggerFactory.getLogger(StartService.class);
 	public static void main(String[] args) {
-		System.out.println("测试");
+		logger.error("测试");
 	}
 }
